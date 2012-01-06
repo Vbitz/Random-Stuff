bfd1bcc41e006cbf90890989f26a92adc4e91de0
Vbitz <jscarsbrook@gmail.com>
2012-01-07 12:58:08 +1300

    The code from the last commit will not work without the static utilty class

---
+++ csharp/NetworkStuff/Singiltons.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SystemsGame
+{
+    static class Singiltons
+    {
+        public static Network LocalNetwork = new Network("Local", false);
+        public static ComputerSystem LocalHost;
+        public static Session LocalSession;
+
+        public static Random rand = new Random();
+        public static List<Network> Networks = new List<Network>();
+
+        private static char[] RandomNameCharactors = "0123456789ABCDEF".ToCharArray();
+
+        public static string GetRandomWord(int length)
+        {
+            string str = "";
+            for (int i = 0; i < length; i++)
+            {
+                str += RandomNameCharactors[rand.Next(RandomNameCharactors.Length)];
+            }
+            return str;
+        }
+
+        public static void GenerateNetworks()
+        {
+            LocalHost = new ComputerSystem(LocalNetwork);
+            LocalNetwork.AllSystems.Add(LocalHost);
+
+            LocalSession = new Session(LocalHost.GetUser("Admin"));
+
+            for (int i = 0; i < 5; i++)
+            {
+                Networks.Add(new Network());
+            }
+        }
+    }
+}
