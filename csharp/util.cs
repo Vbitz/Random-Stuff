da68cac48e688f0ae4fd7465b433d68d503bf0b9
Vbitz <jscarsbrook@gmail.com>
2012-01-07 15:12:29 +1300

    Fixed copyright year

--- csharp/util.cs
+++ csharp/util.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright (C) 2011 by Vbitz
+Copyright (C) 2012 by Vbitz
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
